656c7d4674195d048a1745dc1a2b684b0122daab
sudipmondal7205 <sudipmondal070205@gmail.com>
2026-03-14 00:00:52 +0530

    added history view

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -1,19 +1,73 @@
 import streamlit as st
 from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
+import uuid
 
-CONFIG = {'configurable': {'thread_id': 'thread-1'}}
+
+def generate_thread_id():
+    thread_id = uuid.uuid4()
+    return thread_id
+
+def reset_chat():
+    thread_id = generate_thread_id()
+    st.session_state['thread_id'] = thread_id
+    add_thread(st.session_state['thread_id'])
+    st.session_state['message_history'] = []
+
+def add_thread(thread_id):
+    if thread_id not in st.session_state['chat_thread']:
+        st.session_state['chat_thread'].append(thread_id)
+
+def load_conversation(thread_id):
+    return chatbot.get_state(config={'configurable': {'thread_id': thread_id}}).values['messages']
+
 
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
 
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = generate_thread_id()
+
+if 'chat_thread' not in st.session_state:
+    st.session_state['chat_thread'] = []
+
+add_thread(st.session_state['thread_id'])
+
+
+
+CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+
+
+
+st.sidebar.title("LanhGraph ChatBot")
+
+if st.sidebar.button("New Chat"):
+    reset_chat()
+
+st.sidebar.header("My Conversations")
+
+for thread_id in st.session_state['chat_thread'] :
+    if st.sidebar.button(str(thread_id)):
+        st.session_state['thread_id'] = thread_id
+        messages = load_conversation(thread_id)
+
+        temp_messages = []
+
+        for message in messages:
+            if isinstance(message, HumanMessage):
+                role='user'
+            else:
+                role='ai'
+            temp_messages.append({'role': role, 'content': message.content})
+
+        st.session_state['message_history'] = temp_messages
 
 
 user_input = st.chat_input('Type Here ')
 
 for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
-        st.text(message['content'])
+        st.markdown(message['content'])
 
 if user_input:
 
@@ -25,7 +79,7 @@ if user_input:
         ai_message = st.write_stream(
                 message_chunk.content for message_chunk, metadata in chatbot.stream(
                     {'messages': [HumanMessage(content=user_input)]},
-                    config={'configurable': {'thread_id': 'thread-1'}},
+                    config=CONFIG,
                     stream_mode='messages'
                 )
         )
